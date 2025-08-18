1cc82e220c45d6879d0fdc3f1a1a27fd49e2929d
Zach Kelling <z@zeekay.io>
2025-08-18 17:48:13 +0000

    Sync with node v1.16.15+ - Update Lux CLI for latest compatibility

---
+++ pkg/keychain/wrapper.go
@@ -0,0 +1,60 @@
+// Copyright (C) 2024, Lux Industries Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+package keychain
+
+import (
+	"github.com/luxfi/ids"
+	nodekeychain "github.com/luxfi/node/utils/crypto/keychain"
+	"github.com/luxfi/node/utils/set"
+	"github.com/luxfi/node/vms/secp256k1fx"
+	ledgerkeychain "github.com/luxfi/ledger-lux-go/keychain"
+)
+
+// NodeToLedgerWrapper wraps a node keychain to implement ledger keychain interface
+type NodeToLedgerWrapper struct {
+	nodeKC nodekeychain.Keychain
+}
+
+// WrapNodeKeychain wraps a node keychain to implement ledger keychain interface
+func WrapNodeKeychain(nodeKC nodekeychain.Keychain) ledgerkeychain.Keychain {
+	return &NodeToLedgerWrapper{nodeKC: nodeKC}
+}
+
+// Get returns the signer for the given address
+func (w *NodeToLedgerWrapper) Get(addr ids.ShortID) (ledgerkeychain.Signer, bool) {
+	signer, ok := w.nodeKC.Get(addr)
+	if !ok {
+		return nil, false
+	}
+	// The node signer already implements the ledger signer interface
+	return signer, true
+}
+
+// Addresses returns the addresses managed by this keychain as a slice
+func (w *NodeToLedgerWrapper) Addresses() []ids.ShortID {
+	// Convert set to slice
+	addrSet := w.nodeKC.Addresses()
+	return addrSet.List()
+}
+
+// Secp256k1fxToNodeWrapper wraps a secp256k1fx keychain to implement node keychain interface
+type Secp256k1fxToNodeWrapper struct {
+	secpKC *secp256k1fx.Keychain
+}
+
+// WrapSecp256k1fxKeychain wraps a secp256k1fx keychain to implement node keychain interface
+func WrapSecp256k1fxKeychain(secpKC *secp256k1fx.Keychain) nodekeychain.Keychain {
+	return &Secp256k1fxToNodeWrapper{secpKC: secpKC}
+}
+
+// Get returns the signer for the given address
+func (w *Secp256k1fxToNodeWrapper) Get(addr ids.ShortID) (nodekeychain.Signer, bool) {
+	return w.secpKC.Get(addr)
+}
+
+// Addresses returns the addresses managed by this keychain as a set
+func (w *Secp256k1fxToNodeWrapper) Addresses() set.Set[ids.ShortID] {
+	// Convert slice to set
+	addrs := w.secpKC.Addresses()
+	return set.Of(addrs...)
+}
