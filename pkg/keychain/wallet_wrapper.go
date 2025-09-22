a350fef2e0ba6a46dd22f46ffc67f53a558af0d9
Zach Kelling <z@zeekay.io>
2025-09-22 08:07:53 +0000

    Merge upstream changes from avalanche-cli v1.9.2
    - Updated version from 1.9.0 to 1.9.2
    - Fixed import issues: replaced zap.Field with interface{} methods
    - Fixed keychain interface mismatches between packages
    - Updated set imports from node/utils/set to math/set for consistency
    - Fixed renamed types: SubnetValidator -> NetValidator, Subnet field -> Net
    - Updated wallet methods: IssueTransformSubnetTx -> IssueTransformNetTx
    - Updated builder methods: NewAddSubnetValidatorTx -> NewAddNetValidatorTx
    - Created keychain wrappers to convert between different interfaces
    - Fixed logger adapter to properly implement log.Logger interface
    
    Work in progress - build errors remain in localnet and subnetcmd packages

---
+++ pkg/keychain/wallet_wrapper.go
@@ -0,0 +1,31 @@
+// Copyright (C) 2025, Lux Industries Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+package keychain
+
+import (
+	"github.com/luxfi/ids"
+	"github.com/luxfi/node/utils/crypto/keychain"
+	wallkeychain "github.com/luxfi/node/wallet/keychain"
+)
+
+// CryptoToWalletWrapper wraps a crypto keychain to implement wallet keychain interface
+type CryptoToWalletWrapper struct {
+	cryptoKC keychain.Keychain
+}
+
+// WrapCryptoKeychain wraps a crypto keychain to implement wallet keychain interface
+func WrapCryptoKeychain(cryptoKC keychain.Keychain) wallkeychain.Keychain {
+	return &CryptoToWalletWrapper{cryptoKC: cryptoKC}
+}
+
+// Get returns the signer for the given address
+func (w *CryptoToWalletWrapper) Get(addr ids.ShortID) (wallkeychain.Signer, bool) {
+	return w.cryptoKC.Get(addr)
+}
+
+// Addresses returns the addresses managed by this keychain as a slice
+func (w *CryptoToWalletWrapper) Addresses() []ids.ShortID {
+	// Convert set to slice
+	addrSet := w.cryptoKC.Addresses()
+	return addrSet.List()
+}
