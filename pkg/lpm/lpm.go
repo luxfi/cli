91c9601762692e52bad419c598adc55466a00d27
Zach Kelling <z@zeekay.io>
2025-09-26 02:09:53 +0000

    Update copyright

--- pkg/lpm/lpm.go
+++ pkg/lpm/lpm.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2024, Lux Industries Inc. All rights reserved.
+// Copyright (C) 2022-2025, Lux Industries Inc. All rights reserved.
 // LPM (Lux Plugin Manager) client wrapper for CLI integration
 package lpm
 
