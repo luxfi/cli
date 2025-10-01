fb9e5dd2d4de0a339e39eaead9d6d91091edb7db
Zach Kelling <z@zeekay.io>
2025-10-01 20:55:08 +0000

    Fix more lint and test import issues
    - Fix test imports to use CLI prompts instead of SDK prompts
    - Update migrations_test.go, binutils/release_test.go, export_test.go, lpmintegration/file_test.go
    - Tests should import github.com/luxfi/cli/pkg/prompts for app.Setup calls

--- internal/migrations/migrations_test.go
+++ internal/migrations/migrations_test.go
@@ -9,9 +9,9 @@ import (
 
 	"github.com/luxfi/cli/pkg/application"
 	"github.com/luxfi/cli/pkg/config"
+	"github.com/luxfi/cli/pkg/prompts"
 	"github.com/luxfi/cli/pkg/ux"
 	luxlog "github.com/luxfi/log"
-	"github.com/luxfi/sdk/prompts"
 	"github.com/stretchr/testify/require"
 )
 
