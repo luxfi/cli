4c69282911500e9327120dd04d7ad61bae6f05e3
Zach Kelling <z@zeekay.io>
2025-10-02 19:04:59 +0000

    Run gofmt on test files

--- pkg/apmintegration/file_test.go
+++ pkg/apmintegration/file_test.go
@@ -11,9 +11,9 @@ import (
 	"github.com/luxfi/cli/pkg/application"
 	"github.com/luxfi/cli/pkg/config"
 	"github.com/luxfi/cli/pkg/constants"
+	"github.com/luxfi/cli/pkg/prompts"
 	luxlog "github.com/luxfi/log"
 	"github.com/luxfi/lpm/types"
-	"github.com/luxfi/cli/pkg/prompts"
 	"github.com/stretchr/testify/require"
 )
 
