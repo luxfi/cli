6f934a0ec0ace2f06ae0880ab9d4c899bcc63ee6
Zach Kelling <z@zeekay.io>
2025-10-02 19:04:59 +0000

    Run gofmt on test files

--- internal/migrations/subnetEVMRename_test.go
+++ internal/migrations/subnetEVMRename_test.go
@@ -12,10 +12,10 @@ import (
 	"github.com/luxfi/cli/pkg/application"
 	"github.com/luxfi/cli/pkg/config"
 	"github.com/luxfi/cli/pkg/constants"
+	"github.com/luxfi/cli/pkg/prompts"
 	"github.com/luxfi/cli/pkg/ux"
 	luxlog "github.com/luxfi/log"
 	"github.com/luxfi/sdk/models"
-	"github.com/luxfi/cli/pkg/prompts"
 	"github.com/stretchr/testify/require"
 )
 
