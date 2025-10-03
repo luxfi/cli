ba9fd490137f49cc46fa5a9309206a7084a87760
Zach Kelling <z@zeekay.io>
2025-10-03 04:45:14 +0000

    Fix all remaining CI errors with parallel CTO agents
    - Fix PrintToUser non-constant format string errors properly
    - Fix config.LuxDataDirVar renamed to LuxNodeDataDirVar
    - Fix mock interface issues between CLI and SDK prompts
    - Fix address type conversions (geth Address vs crypto Address)
    - Add SDKPrompter mock for SDK interface compatibility
    - Fix networkID type issues in tests
    - Add Q-Chain support infrastructure
    
    All test compilation issues resolved.

--- pkg/vm/airdrop_test.go
+++ pkg/vm/airdrop_test.go
@@ -10,16 +10,18 @@ import (
 	"github.com/luxfi/cli/internal/mocks"
 	"github.com/luxfi/cli/pkg/application"
 	"github.com/luxfi/cli/pkg/statemachine"
+	"github.com/luxfi/crypto"
 	"github.com/luxfi/geth/common"
 	"github.com/stretchr/testify/mock"
 )
 
 var testAirdropAddress = common.HexToAddress("0x098B69E43b1720Bd12378225519d74e5F3aD0eA5")
+var testAirdropCryptoAddress = crypto.BytesToAddress(testAirdropAddress.Bytes())
 
 func TestGetAllocationCustomUnits(t *testing.T) {
 	require := setupTest(t)
 	app := application.New()
-	mockPrompt := &mocks.Prompter{}
+	mockPrompt := &mocks.SDKPrompter{}
 	app.Prompt = mockPrompt
 
 	airdropInputAmount := new(big.Int)
@@ -29,7 +31,7 @@ func TestGetAllocationCustomUnits(t *testing.T) {
 	expectedAmount.SetString(defaultEvmAirdropAmount, 10)
 
 	mockPrompt.On("CaptureList", mock.Anything, mock.Anything).Return(customAirdrop, nil)
-	mockPrompt.On("CaptureAddress", mock.Anything).Return(testAirdropAddress, nil)
+	mockPrompt.On("CaptureAddress", mock.Anything).Return(testAirdropCryptoAddress, nil)
 	mockPrompt.On("CapturePositiveBigInt", mock.Anything).Return(airdropInputAmount, nil)
 	mockPrompt.On("CaptureNoYes", mock.Anything).Return(false, nil)
 
