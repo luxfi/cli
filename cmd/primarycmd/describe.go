e6734e82e1f7698bf8a8a018e3f108b570ba2677
Hanzo Dev <dev@hanzo.ai>
2025-12-04 18:00:24 -0800

    Fix primarycmd constants

--- cmd/primarycmd/describe.go
+++ cmd/primarycmd/describe.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/luxfi/cli/pkg/cobrautils"
 	"github.com/luxfi/cli/pkg/key"
+	"github.com/luxfi/cli/pkg/localnet"
 	"github.com/luxfi/cli/pkg/networkoptions"
 	"github.com/luxfi/cli/pkg/utils"
 	"github.com/luxfi/cli/pkg/ux"
