490bac44b8a4f820bc1323952f54d421a6d8448d
Zach Kelling <z@zeekay.ai>
2025-12-29 14:02:30 -0800

    refactor: use github.com/luxfi/const (renamed from constants)

--- pkg/key/key.go
+++ pkg/key/key.go
@@ -9,7 +9,7 @@ import (
 	"errors"
 	"sort"
 
-	"github.com/luxfi/constants"
+	"github.com/luxfi/const"
 	"github.com/luxfi/ids"
 	"github.com/luxfi/node/vms/components/lux"
 	"github.com/luxfi/node/vms/platformvm/txs"
