516af560e1be1973f04acfb4dcaa42f615a0d64b
Zach Kelling <z@zeekay.io>
2026-01-01 18:42:58 -0800

    chore: exclude pkg/ssh/shell from shellcheck
    These are Go template files using {{ .Variable }} syntax that
    get processed by text/template before shell execution.

--- scripts/shellcheck.sh
+++ scripts/shellcheck.sh
@@ -3,7 +3,15 @@
 set -e
 
 # Find all shell scripts and run shellcheck on them
+# Excludes:
+#   - vendor directory
+#   - .git directory
+#   - pkg/ssh/shell/ - Go template files using {{ .Variable }} syntax
 echo "Running shellcheck..."
-find . -name "*.sh" -type f -not -path "./vendor/*" -not -path "./.git/*" | xargs shellcheck
+find . -name "*.sh" -type f \
+    -not -path "./vendor/*" \
+    -not -path "./.git/*" \
+    -not -path "./pkg/ssh/shell/*" \
+    | xargs shellcheck
 
 echo "Shellcheck passed!"
