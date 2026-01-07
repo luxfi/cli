63b77ce76080f65d5b7a9880c67f480717af9cfb
Zach Kelling <z@zeekay.io>
2026-01-06 22:17:58 -0800

    Improve command output

---
+++ pkg/status/models.go
@@ -0,0 +1,99 @@
+package status
+
+import (
+	"time"
+)
+
+// Network represents a Lux network (mainnet, testnet, devnet, custom)
+type Network struct {
+	Name      string
+	Nodes     []Node
+	Chains    []ChainStatus
+	Endpoints []EndpointStatus
+	Metadata  NetworkMetadata
+}
+
+// NetworkMetadata contains additional network information
+type NetworkMetadata struct {
+	GRPCPort    int
+	NodesCount  int
+	VMsCount    int
+	Controller  string // "on" or "off"
+	Status      string // "up", "down", "stopped"
+}
+
+// Node represents a network node
+type Node struct {
+	ID          string
+	HTTPURL     string
+	Version     string
+	PeerCount   int
+	Uptime      string
+	OK          bool
+	LatencyMS   int
+	LastError   string
+}
+
+// ChainStatus represents the status of a chain
+type ChainStatus struct {
+	Alias      string // "c", "p", "x", "dex", etc.
+	Kind       string // "evm", "pchain", "xchain", "custom"
+	Height     uint64
+	BlockTime  *time.Time
+	RPC_OK     bool
+	LatencyMS  int
+	ChainID    string
+	Syncing    interface{} // bool or sync progress object
+	Metadata   map[string]interface{}
+	LastError  string
+}
+
+// EndpointStatus represents the status of an RPC endpoint
+type EndpointStatus struct {
+	ChainAlias string
+	URL        string
+	OK         bool
+	LatencyMS  int
+	LastError  string
+}
+
+// TrackedEVM represents a tracked EVM chain (Zoo, Hanzo, SPC, etc.)
+type TrackedEVM struct {
+	Name          string // zoo, hanzo, spc
+	Network       string // mainnet, testnet
+	RPCs          []string
+	BlockchainID  string // if available
+	VMID          string // if available
+}
+
+// EVMStatus represents the status of a tracked EVM
+type EVMStatus struct {
+	Name           string
+	Network        string
+	ChainID        uint64
+	Height         uint64
+	LatestTime     *time.Time
+	Syncing        interface{} // bool or sync progress object
+	ClientVersion  string
+	PluginVersion  string
+	Endpoints      []EndpointStatus
+	DriftDetected  bool
+	ChainIDMismatch bool
+}
+
+// StatusResult contains the complete status information
+type StatusResult struct {
+	Networks    []Network
+	TrackedEVMs []EVMStatus
+	Timestamp   time.Time
+	DurationMS  int
+}
+
+// ProbeResult contains the result of a single probe
+type ProbeResult struct {
+	OK         bool
+	LatencyMS  int
+	Height     uint64
+	Meta       map[string]interface{}
+	Error      error
+}
